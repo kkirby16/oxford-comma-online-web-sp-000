bfb39bb712ae683c58c948f904ddae3c606a9784
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-09 12:45:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.size == 2 
     array.join(" and ")
-    elsif array.size <= 3
+    elsif array.size >= 3
     array[0..-2].join(", ") + ", and " + array[-1]
 end
 end
