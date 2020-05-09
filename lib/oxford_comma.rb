fed615fa33edb664a72f92cedd30ac67547f84fd
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-09 12:50:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
   elsif array.size >= 3
-    array[0..-2].join(", ") + ", and " + array[-1]
+    array[0..-2].join(", ") + ", and " + array.lasts
   end
 end
